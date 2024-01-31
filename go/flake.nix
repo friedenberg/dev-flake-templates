7385b40b60dd263ee0cd9d78e618d66a0a7bed51
Sasha Friedenberg <cornflake.matchbox397@icantbelieveitsnotgmail.com>
2024-01-31 13:48:54 -0500

    add go and php

--- go/flake.nix
+++ go/flake.nix
@@ -1,31 +1,33 @@
 {
-  description = "A Nix-flake-based Go 1.17 development environment";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    nixpkgs-master.url = "github:NixOS/nixpkgs/master";
+    utils.url = "github:numtide/flake-utils";
 
-  inputs.nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1.*.tar.gz";
-
-  outputs = { self, nixpkgs }:
-    let
-      goVersion = 20; # Change this to update the whole stack
-      overlays = [ (final: prev: { go = prev."go_1_${toString goVersion}"; }) ];
-      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
-      forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
-        pkgs = import nixpkgs { inherit overlays system; };
-      });
-    in
-    {
-      devShells = forEachSupportedSystem ({ pkgs }: {
-        default = pkgs.mkShell {
-          packages = with pkgs; [
-            # go 1.20 (specified by overlay)
-            go
-
-            # goimports, godoc, etc.
-            gotools
-
-            # https://github.com/golangci/golangci-lint
-            golangci-lint
-          ];
-        };
-      });
+    gomod2nix = {
+      url = "github:nix-community/gomod2nix";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
+  };
+
+  outputs = { self, nixpkgs, utils, nixpkgs-master, gomod2nix}:
+    (utils.lib.eachDefaultSystem
+      (system:
+        let pkgs = import nixpkgs {
+          inherit system;
+        }; 
+
+        in
+
+        rec {
+          devShells.default = pkgs.mkShell {
+            packages = with pkgs; [
+              go
+              golangci-lint
+              gopls
+              gotools
+            ];
+          };
+        })
+    );
 }
