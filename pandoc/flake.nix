faa62d29eb9121c0da99595fa265f82d854845f3
Sasha Friedenberg <cornflake.matchbox397@icantbelieveitsnotgmail.com>
2024-01-31 13:53:36 -0500

    add pandoc

---
+++ pandoc/flake.nix
@@ -0,0 +1,25 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    nixpkgs-master.url = "github:NixOS/nixpkgs/master";
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, utils, nixpkgs-master}:
+    (utils.lib.eachDefaultSystem
+      (system:
+        let pkgs = import nixpkgs {
+          inherit system;
+        };
+
+        in
+
+        rec {
+          devShells.default = pkgs.mkShell {
+            packages = with pkgs; [
+              pandoc
+            ];
+          };
+        })
+    );
+}
