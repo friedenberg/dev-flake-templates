c23070f445ec1d306f91c01fe2a3e525813fc9a3
Sasha Friedenberg <cornflake.matchbox397@icantbelieveitsnotgmail.com>
2024-06-11 07:45:59 -0400

    update

--- javascript/flake.nix
+++ javascript/flake.nix
@@ -3,14 +3,9 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
     nixpkgs-master.url = "github:NixOS/nixpkgs/master";
     utils.url = "github:numtide/flake-utils";
-
-    gomod2nix = {
-      url = "github:nix-community/gomod2nix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
   };
 
-  outputs = { self, nixpkgs, utils, nixpkgs-master, gomod2nix}:
+  outputs = { self, nixpkgs, utils, nixpkgs-master}:
     (utils.lib.eachDefaultSystem
       (system:
         let pkgs = import nixpkgs {
@@ -22,10 +17,8 @@
         rec {
           devShells.default = pkgs.mkShell {
             packages = with pkgs; [
-              go
-              golangci-lint
-              gopls
-              gotools
+              nixpkgs.nodePackages.typescript-language-server
+              nixpkgs.nodePackages.typescript
             ];
           };
         })
--- php/flake.nix
+++ php/flake.nix
@@ -19,6 +19,7 @@
             packages = with pkgs; [
               php
               phpPackages.composer
+              phpactor
             ];
           };
         })
