0c6754c44ff87db0f694bac883be1f1195557fa3
Sasha F <cornflake.matchbox397@icantbelieveitsnotgmail.com>
2024-12-28 18:13:54 -0500

    update

--- nix/flake.nix
+++ nix/flake.nix
@@ -2,27 +2,36 @@
   description = "A Nix-flake-based Nix development environment";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs-stable.url = "nixpkgs/release-24.11";
     utils.url = "github:numtide/flake-utils";
+    fh.url = "https://flakehub.com/f/DeterminateSystems/fh/0.1.21.tar.gz";
   };
 
-  outputs = { self, nixpkgs, utils }:
+  outputs =
+    { self
+    , nixpkgs
+    , nixpkgs-stable
+    , utils
+    , fh
+    }:
     (utils.lib.eachDefaultSystem
       (system:
-        let
-          pkgs = import nixpkgs {
-            inherit system;
-          };
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
 
-        in
+      in
 
-        {
-          devShells.default = pkgs.mkShell {
-            packages = with pkgs; [
-              nil
-              nixfmt-rfc-style
-            ];
-          };
-        })
+      {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            fh.packages.${system}.default
+            nil
+            nixfmt-rfc-style
+          ];
+        };
+      })
     );
 }
